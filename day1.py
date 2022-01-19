ed2701ebee295d6bffbbe0ab1cfb50c5baaaaf13
Gytis Kairys <gytis.kairys@gmail.com>
2022-01-19 23:44:38 +0000

    got input working using aocd library

--- day1.py
+++ day1.py
@@ -1,7 +1,23 @@
-n = 5
-y = 5
-r = n + y
-print(r)
 import numpy as np
 import pandas as pd
-f
+
+import urllib
+
+import requests
+import os
+link = "https://adventofcode.com/2021/day/1/input"
+f = requests.get(link)
+#print(f.text)
+print(os.environ)
+import aocd
+
+#aocd.cookies.scrape_session_tokens()
+#print(aocd(scrape_session_tokens))
+#
+from aocd import data
+from aocd import get_data
+y = get_data(day=1, year=2021)
+print(y)
+
+#export AOC_SESSION=cafef00db01dfaceba5eba11deadbeef
+#from aocd import data
