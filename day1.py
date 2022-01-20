495b70495e06ab70d375c9ee734263ecf40991f3
Gytis Kairys <gytis.kairys@gmail.com>
2022-01-20 23:31:25 +0000

    Both solutions to 1 and 2

--- day1.py
+++ day1.py
@@ -11,14 +11,32 @@ l = raw_data.splitlines()
 
 #converting to numbers:
 l = [int(n) for n in l]
+
+#using numpy diff function to get
+#difference between one value and previous value
 res = np.diff(l,1)
+
+#finding out all increases:
 res = res[res[:] > 0]
-final_result = res.size
-print(final_result)
 
-#initiate pandas list of values:
-#list_data = pd.Series(array_raw_data.transpose())
-#print(list_data)
+# getting final result by simply getting
+# size of this 1d array:
+final_result_part1 = res.size
+print("Results for day1 part 1: " + str(final_result_part1))
 
-#export AOC_SESSION=cafef00db01dfaceba5eba11deadbeef
-#from aocd import data
+# Part 2
+loop_count = 0
+sum3_array = [0] * (len(l)-2) #np.zeros(len(l)-2)
+for i in l:
+    sum3 = l[loop_count] + l[loop_count+1] + l[loop_count+2]
+    sum3_array[loop_count] = sum3
+    loop_count += 1
+    #break the sums at the end
+    if loop_count == len(l)-2:
+        break
+
+#similarly as in part 1, finding increases:
+res_sum3 = np.diff(sum3_array,1)
+res_sum3 = res_sum3[res_sum3[:] > 0]
+final_result_part2 = res_sum3.size
+print("results for day1 part 2: " + str(final_result_part2))
