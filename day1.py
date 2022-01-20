bd3ba1609570e036750fae7afd1a9facdcc212e2
Gytis Kairys <gytis.kairys@gmail.com>
2022-01-20 22:35:29 +0000

    Part 1 solution

--- day1.py
+++ day1.py
@@ -1,23 +1,24 @@
 import numpy as np
 import pandas as pd
-
 import urllib
-
-import requests
-import os
-link = "https://adventofcode.com/2021/day/1/input"
-f = requests.get(link)
-#print(f.text)
-print(os.environ)
 import aocd
-
-#aocd.cookies.scrape_session_tokens()
-#print(aocd(scrape_session_tokens))
-#
-from aocd import data
+# getting data from aoc:
 from aocd import get_data
-y = get_data(day=1, year=2021)
-print(y)
+raw_data = get_data(day=1, year=2021)
+
+# splitting data into an array
+l = raw_data.splitlines()
+
+#converting to numbers:
+l = [int(n) for n in l]
+res = np.diff(l,1)
+res = res[res[:] > 0]
+final_result = res.size
+print(final_result)
+
+#initiate pandas list of values:
+#list_data = pd.Series(array_raw_data.transpose())
+#print(list_data)
 
 #export AOC_SESSION=cafef00db01dfaceba5eba11deadbeef
 #from aocd import data
